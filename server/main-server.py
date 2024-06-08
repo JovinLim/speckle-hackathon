5880162fcbd0735804624788e3d71ae7140661cd
Jovin Lim <jovinlim@dpa.com.sg>
2024-06-08 11:43:51 +0800

    api done

--- server/main-server.py
+++ server/main-server.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 import requests
 import os
 from dotenv import load_dotenv, dotenv_values
+from utils.excel_utils import check_type_mark, update_excel, FamilyDatabaseParams
+import subprocess
 
 app = FastAPI()
 
@@ -29,6 +31,14 @@ class QueryRequest(BaseModel):
     query: str
     token: str
 
+class UpdateRequest(BaseModel):
+    family_params: FamilyDatabaseParams
+    stream: str
+    commit: str
+
+class MatchRequest(BaseModel):
+    family_params: FamilyDatabaseParams
+
 # Load environment variables
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env.' + os.environ.get('NODE_ENV', 'development'))
 load_dotenv(dotenv_path)
@@ -37,6 +47,8 @@ SPECKLE_URL = os.getenv('VITE_SPECKLE_URL')
 APP_SPECKLE_ID = os.getenv('VITE_SPECKLE_ID')
 APP_SPECKLE_NAME = os.getenv('VITE_SPECKLE_NAME')
 APP_SPECKLE_SECRET = os.getenv('VITE_SPECKLE_SECRET')
+APP_EXCEL_DATABASE_WINDOWS = os.getenv('APP_EXCEL_DATABASE') + '.xlsx'
+APP_EXCEL_DATABASE_WSL = subprocess.check_output(['wslpath', APP_EXCEL_DATABASE_WINDOWS]).decode().strip()
 
 if not SPECKLE_URL or not APP_SPECKLE_ID or not APP_SPECKLE_NAME or not APP_SPECKLE_SECRET:
     raise EnvironmentError("Missing one or more required environment variables")
@@ -77,6 +89,85 @@ def speckle_query(request: QueryRequest):
         return response_data
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/match_family")
+def match_family(request: MatchRequest):
+    response_data = {"Response : Something went wrong with graphql"}
+
+    # refObj_query = {
+    #     "query" : '''
+    #                 query {{
+    #                     stream(id: "{0}") {{
+    #                         commit(id: "{1}") {{
+    #                             referencedObject
+    #                         }}
+    #                     }}
+    #                 }}
+    #             '''.format(request.stream, request.commit)
+    # }
+
+    # headers = {
+    #     "Authorization": f"Bearer {request.token}",
+    #     "Content-Type": "application/json"
+    # }
+
+    # query_url = f"{SPECKLE_URL}/graphql"
+
+    # # OBTAIN REFERENCE OBJECT FROM COMMIT ID
+    # try:
+    #     response = requests.post(query_url, headers=headers, json=refObj_query)
+    #     response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
+    #     response_data = response.json()
+    #     refObj = response_data['data']['stream']['commit']['referencedObject']
+    #     if refObj == None or refObj == "":
+    #         return response_data
+    #     print (refObj)
+
+    # except requests.exceptions.RequestException as e:
+    #     raise HTTPException(status_code=500, detail=str(e))
+    
+    # OBTAIN TYPEMARK INFORMATION
+    try:
+        fam_params = request.family_params
+        match_result = check_type_mark(filepath=APP_EXCEL_DATABASE_WSL, family_params=fam_params)
+        if match_result:
+            print(f"The corresponding filepath is: {match_result}")
+        else:
+            print("No matching type mark found or an error occurred.")
+
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    return response_data
+    
+@app.post("/update_database")
+def update_database(request: UpdateRequest):
+    response_data = {"Response" : "Something went wrong with database update."}
+
+    if request.stream == "" or request.stream == None:
+        response_data = {"Response" : "Unable to detect stream in request parameters."}
+        return response_data
+    
+    if request.commit == "" or request.commit == None:
+        response_data = {"Response" : "Unable to detect commit in request parameters."}
+        return response_data
+    
+    try:
+        fam_params = request.family_params
+        match_result = check_type_mark(filepath=APP_EXCEL_DATABASE_WSL, family_params=fam_params)
+        if match_result:
+            print(f"Family type already exists in database.")
+            response_data["Response"] = "Family type mark already exists in database."
+            return response_data
+        else:
+            update_excel(filepath=APP_EXCEL_DATABASE_WSL, new_data=fam_params)
+            response_data["Response"] = "Successfully updated database."
+            return response_data
+        pass
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    return response_data
 
 if __name__ == "__main__":
     import uvicorn
